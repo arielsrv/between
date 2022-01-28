14eea708cfdaad19113d0ae6a4e0768e3f4da84e
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-28 19:32:52 +0100

    fx

--- src/test/java/com/between/BetweenApplicationTests.java
+++ src/test/java/com/between/BetweenApplicationTests.java
@@ -3,11 +3,13 @@ package com.between;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class BetweenApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
-
 }
