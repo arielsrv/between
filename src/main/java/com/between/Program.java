f3dc799b1c6095b4226881dad4c13d1451e9d74d
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-28 18:42:14 +0100

    fix

--- src/main/java/com/between/Program.java
+++ src/main/java/com/between/Program.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BetweenApplication {
+public class Program {
 
     public static void main(String[] args) {
-        SpringApplication.run(BetweenApplication.class, args);
+        SpringApplication.run(Program.class, args);
     }
 
 }
