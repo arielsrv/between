891aef3b01ea004ca6f0878794612b56f76849ff
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-28 19:56:04 +0100

    fix

--- src/test/java/com/between/unit/PingControllerTest.java
+++ src/test/java/com/between/unit/PingControllerTest.java
@@ -1,2 +1,19 @@
-package com.between.unit;public class PingControllerTest {
+package com.between.unit;
+
+import com.between.controllers.PingController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class PingControllerTest {
+
+    @Test
+    public void ping() {
+        PingController pingController = new PingController();
+        String actual = pingController.pong();
+
+        assertThat(actual).isEqualTo("pong");
+    }
 }
