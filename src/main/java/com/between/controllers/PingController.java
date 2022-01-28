3f31f9263af8b33482500eca85e51a72ad737460
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-28 18:25:56 +0100

    missing files

--- src/main/java/com/between/controllers/PingController.java
+++ src/main/java/com/between/controllers/PingController.java
@@ -1,2 +1,16 @@
-package com.between.controllers;public class PingController {
+package com.between.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public @ResponseBody
+    String pong() {
+        return "pong";
+    }
 }
