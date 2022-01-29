63dfe5b1ae40e9314ca733c76e1d33d3ed98218a
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-29 10:13:41 +0100

    minor

--- src/main/java/com/between/controllers/PriceController.java
+++ src/main/java/com/between/controllers/PriceController.java
@@ -5,6 +5,7 @@ import com.between.services.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,7 +22,8 @@ public class PriceController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ArrayList<PriceDto> getAll() {
+    public @ResponseBody
+    ArrayList<PriceDto> getAll() {
         return this.priceService.getAll();
     }
 }
