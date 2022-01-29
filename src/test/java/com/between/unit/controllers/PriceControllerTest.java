188595b29be6d33f3a49ff27bd3979719260b227
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-29 10:21:07 +0100

    test

---
+++ src/test/java/com/between/unit/controllers/PriceControllerTest.java
@@ -0,0 +1,49 @@
+package com.between.unit.services;
+
+import com.between.controllers.PriceController;
+import com.between.dtos.PriceDto;
+import com.between.services.PriceService;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class PriceControllerTest {
+
+    private PriceService priceService;
+    private PriceController priceController;
+
+    @BeforeEach
+    void setUp() {
+        this.priceService = mock(PriceService.class);
+        this.priceController = new PriceController(this.priceService);
+    }
+
+    @Test
+    void get_item_ok() {
+        when(this.priceService.getAll()).thenReturn(getItems());
+        ArrayList<PriceDto> actual = this.priceController.getAll();
+        assertThat(actual).isNotNull();
+        assertThat(actual).isNotEmpty();
+        assertThat(actual.stream().findFirst().isPresent()).isTrue();
+        assertThat(actual.stream().findFirst().get().productId).isEqualTo(1L);
+    }
+
+    private ArrayList<PriceDto> getItems() {
+        ArrayList<PriceDto> priceDtos = new ArrayList<>();
+
+        PriceDto priceDto = new PriceDto();
+        priceDto.productId = 1L;
+
+        priceDtos.add(priceDto);
+
+        return priceDtos;
+    }
+}
