864899bafd1fe6bf035a207da92540b93d5c5614
Ariel Piñeiro <arielsrv@gmail.com>
2022-01-30 11:11:15 +0100

    singleton

--- src/main/java/com/between/repositories/IProductRepository.java
+++ src/main/java/com/between/repositories/IProductRepository.java
@@ -1,8 +1,15 @@
 package com.between.repositories;
 
 import com.between.entities.Product;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public interface IProductRepository extends CrudRepository<Product, Long> {
 
 }
